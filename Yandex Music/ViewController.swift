8c1c87b59382336393bd5c19c82cb71d34a65b2b
Sergey Moskvin <s@mosk.vin>
2022-02-11 00:04:15 +0300

    Создан новый проект

---
+++ Yandex Music/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Yandex Music
+//
+//  Created by Sergey Moskvin on 11.02.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
