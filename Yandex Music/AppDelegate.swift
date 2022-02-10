083310d551aa98c82c92369516e482c333b5b215
Sergey Moskvin <s@mosk.vin>
2022-02-11 00:17:26 +0300

    Выполнена небольшая очистка

--- Yandex Music/AppDelegate.swift
+++ Yandex Music/AppDelegate.swift
@@ -7,24 +7,6 @@
 
 import Cocoa
 
-@main
-class AppDelegate: NSObject, NSApplicationDelegate {
-
+@main final class AppDelegate: NSObject, NSApplicationDelegate {
     
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-
 }
-
