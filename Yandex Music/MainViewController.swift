b9781a219f6cd4685cf1bd21ef3990bea45ce9dd
Georgy Kovalenko <b@tcoh.ru>
2022-02-28 00:32:04 +0300

    JS код для работы виджета вынесен в отдельный файл

--- Yandex Music/MainViewController.swift
+++ Yandex Music/MainViewController.swift
@@ -124,52 +124,11 @@ extension MainViewController: WKNavigationDelegate {
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        webView.evaluateJavaScript("""
-            if ('mediaSession' in navigator && externalAPI) {
-                navigator.mediaSession.setActionHandler('play', () => {
-                    externalAPI.togglePause(false);
-                });
-                navigator.mediaSession.setActionHandler('pause', () => {
-                    externalAPI.togglePause(true);
-                });
-                navigator.mediaSession.setActionHandler('seekto', ({ seekTime }) => {
-                    externalAPI.setPosition(seekTime);
-                });
-                const updateControls = () => {
-                    const controls = externalAPI.getControls();
-                    navigator.mediaSession.setActionHandler("previoustrack", controls.prev ? externalAPI.prev : null);
-                    navigator.mediaSession.setActionHandler("nexttrack", controls.next ? externalAPI.next : null);
-                }
-                externalAPI.on(externalAPI.EVENT_CONTROLS, updateControls);
-                updateControls();
-
-                externalAPI.on(externalAPI.EVENT_TRACK, () => {
-                    const track = externalAPI.getCurrentTrack();
-                    navigator.mediaSession.metadata = new MediaMetadata({
-                        title: track.title || "",
-                        artist: track.artists.map((a) => a.title).join(", "),
-                        album: track.album ? track.album.title : "",
-                        artwork: track.cover ? [{
-                            src: "https://"+track.cover.replace("%%", "200x200"),
-                            sizes: "200x200",
-                            type: "image/jpeg"
-                        }] : []
-                    });
-                });
-
-                externalAPI.on(externalAPI.EVENT_PROGRESS, () => {
-                    const progress = externalAPI.getProgress();
-                    navigator.mediaSession.setPositionState({
-                        duration: progress.duration,
-                        playbackRate: externalAPI.getSpeed(),
-                        position: progress.position
-                    });
-                });
-                externalAPI.on(externalAPI.EVENT_STATE, () => {
-                    navigator.mediaSession.playbackState = externalAPI.isPlaying() ? "playing" : "paused";
-                });
-            }
-        """)
+        let path = Bundle.main.path(forResource: "init", ofType: "js")!
+        let initJS = try? String(contentsOfFile: path, encoding: String.Encoding.utf8)
+        if (initJS != nil) {
+            webView.evaluateJavaScript(initJS!)
+        }
     }
     
 }
